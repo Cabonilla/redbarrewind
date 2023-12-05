b1057e38b78fd9410b077a35552f78596f1e2e2d
Cabonilla <christian.bonilla01@gmail.com>
2023-12-05 14:19:09 -0800

    MVP working. Debugging and cleaning up code.

--- background.js
+++ background.js
@@ -7,16 +7,6 @@ function getActiveTabId(callback) {
   });
 }
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.command === "checkActiveTab") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const isValid = tabs && tabs.length > 0;
-      sendResponse({ isValid: isValid });
-    });
-    return true; // Indicates that sendResponse will be called asynchronously
-  }
-});
-
 chrome.runtime.onMessage.addListener(async function (
   message,
   sender,
@@ -25,48 +15,66 @@ chrome.runtime.onMessage.addListener(async function (
   getActiveTabId((tabId) => {
     if (message.command === "jumpToTime") {
       console.log("message sent: jumpToTime");
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tabId },
-          func: (time) => {
-            const videoElement = document.querySelector("video");
-            if (videoElement) {
-              console.log("total time: ", time);
-              videoElement.currentTime = time;
+      chrome.runtime?.id
+        ? chrome.scripting.executeScript(
+            {
+              target: { tabId: tabId },
+              func: (time) => {
+                const videoElement = document.querySelector("video");
+                if (videoElement) {
+                  console.log("total time: ", time);
+                  videoElement.currentTime = time;
+                }
+              },
+              args: [message.time], // Pass the time parameter
+            },
+            (result) => {
+              if (chrome.runtime.lastError) {
+                console.error(
+                  "Background script: Error executing script in content script:",
+                  chrome.runtime.lastError
+                );
+              } else {
+                console.log(
+                  "Background script: Script executed successfully:",
+                  result
+                );
+              }
             }
-          },
-          args: [message.time], // Pass the time parameter
-        },
-        (result) => {
-          if (chrome.runtime.lastError) {
-            console.error(
-              "Background script: Error executing script in content script:",
-              chrome.runtime.lastError
-            );
-          } else {
-            console.log(
-              "Background script: Script executed successfully:",
-              result
-            );
-          }
-        }
-      );
+          )
+        : null;
     } else if (message.command === "getLink") {
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        func: (time) => {
-          const pageUrl = window.location.href
-            .replace("www.", "")
-            .replace("youtube.com", "youtu.be")
-            .replace("watch?v=", "");
-          // console.log(pageUrl + "?feature=shared&t=" + Math.ceil(time))
-          // console.log(time)
-          navigator.clipboard.writeText(
-            pageUrl + "?feature=shared&t=" + Math.ceil(time)
-          );
-        },
-        args: [message.time], // Pass the time parameter
-      });
+      chrome.runtime?.id
+        ? chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            func: (time) => {
+              if (window.location.href.includes("youtube")) {
+                const pageUrl = window.location.href
+                  .replace("www.", "")
+                  .replace("youtube.com", "youtu.be")
+                  .replace("watch?v=", "");
+                navigator.clipboard.writeText(
+                  pageUrl + "?feature=shared&t=" + Math.ceil(time)
+                );
+              } else {
+                const pageUrl = window.location.href;
+                navigator.clipboard.writeText(pageUrl);
+              }
+            },
+            args: [message.time],
+          })
+        : null;
+    } else if (message.command === "copyTime") {
+      console.log("time, copied");
+      chrome.runtime?.id
+        ? chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            func: (time) => {
+              navigator.clipboard.writeText(time);
+            },
+            args: [message.time],
+          })
+        : null;
     }
   });
   return true;
--- content.js
+++ content.js
@@ -1,100 +1,108 @@
 console.log("content.js");
-let overlayVisibleBool = false;
 
-// Function to handle overlay display
-function toggleOverlay(overlayVisible) {
-  chrome.runtime.sendMessage(
-    { command: "checkActiveTab" },
-    function (response) {
-      if (response && response.isValid) {
-        const videoElement = document.getElementsByTagName("video")[0];
-        const popup = document.getElementById("rr_overlay");
+(function () {
+  if (window.hasRun === true) return true;
+  window.hasRun = true;
 
-        if (videoElement && popup) {
-          const videoRect = videoElement.getBoundingClientRect();
+  let overlayVisibleBool = false;
 
-          overlayVisible = !overlayVisible;
+  let domain = window.location.href;
 
-          if (!overlayVisible) {
-            console.log(videoRect);
-            popup.style.display = "flex";
-            popup.style.position = "absolute";
-            popup.style.left = videoRect.left + "px";
-            popup.style.bottom = videoRect.bottom + "px";
-            popup.style.top = videoRect.top + "px";
-            popup.style.width = videoRect.width + "px";
-            popup.style.height = videoRect.height + "px";
-            
-            popup.style.zIndex = 9999;
+  function toggleOverlay(overlayVisible) {
+    const videoElement = document.getElementsByTagName("video")[0];
+    const popup = document.getElementById("rr_overlay");
+    const inputElement = document.getElementById("timeInput");
 
-            popup.style.justifyContent = "center";
-            popup.style.alignItems = "center";
-            popup.style.flexDirection = "column";
+    if (videoElement && popup) {
+      const videoRect = videoElement.getBoundingClientRect();
+      const myInput = document.getElementById("timeInput");
 
-            const timeInput = document.getElementById("timeInput");
-            if (timeInput) {
-              timeInput.focus();
-            }
-          } else {
-            // Hide the overlay
-            popup.style.display = "none";
-          }
+      overlayVisible = !overlayVisible;
+
+      if (!overlayVisible) {
+        console.log(videoRect);
+        popup.style.display = "flex";
+        popup.style.position = "absolute";
+        popup.style.left = videoRect.left + "px";
+        popup.style.bottom = videoRect.bottom + "px";
+        popup.style.top = videoRect.top + "px";
+        popup.style.width = videoRect.width + "px";
+        popup.style.height = videoRect.height + "px";
+
+        popup.style.zIndex = 9999;
+
+        popup.style.justifyContent = "center";
+        popup.style.alignItems = "center";
+        popup.style.flexDirection = "column";
+
+        const timeInput = document.getElementById("timeInput");
+        if (timeInput) {
+          timeInput.focus();
         }
+      } else {
+        popup.style.display = "none";
       }
     }
-  );
-}
+  }
 
-window.onresize = function () {
-  const videoElement = document.querySelector("video");
-  const videoRect = videoElement.getBoundingClientRect();
-  const popup = document.getElementById("rr_overlay");
-  popup.style.left = videoRect.left + "px";
-  popup.style.bottom = videoRect.bottom + "px";
-  popup.style.top = videoRect.top + "px";
-  popup.style.width = videoRect.width + "px";
-  popup.style.height = videoRect.height + "px";
-};
+  window.onresize = function () {
+    const videoElement = document.querySelector("video");
+    const videoRect = videoElement.getBoundingClientRect();
+    const popup = document.getElementById("rr_overlay");
+    popup.style.left = videoRect.left + "px";
+    popup.style.bottom = videoRect.bottom + "px";
+    popup.style.top = videoRect.top + "px";
+    popup.style.width = videoRect.width + "px";
+    popup.style.height = videoRect.height + "px";
+  };
 
-let overlay_style = `
+  let overlay_style = `
   display: none; 
   position: fixed; 
   top: 0; 
   right: 0; 
   bottom: 0; 
   left: 0; 
-  background-color: rgba(0, 0, 0, 0.75);
+  background-color: rgba(0, 0, 0, 0.5);
+  clear: both;
 `;
 
-let image_style = `
+  let image_style = `
   width: 200px;
   height: auto;
   margin-top: -50px;
   margin-bottom: -15px;
 `;
 
-let link_logo_style = `
+  let link_logo_style = `
+  width: 12px;
+  height: auto;
+`;
+  let time_logo_style = `
   width: 12px;
   height: auto;
 `;
 
-let input_style = `
+  let input_style = `
   border: 0px solid;
   background-color: transparent;
   color: white;
   font-size: 24px;
   padding: 10px;
-  width: 120px;
+  width: 10vw;
+  padding: .5em;
   outline: none;
   font-family: Arial, sans-serif;
+  z-index: 99999 !important;
+  position: relative;
 `;
 
-let form_style = `
+  let form_style = `
   display: flex;
   align-items: center;
 `;
 
-let button_style = `
+  let button_style = `
   width: 100px;
   height: 30px;
   line-height: 20px;
@@ -108,13 +116,26 @@ let button_style = `
   color: white;
 `;
 
-let link_button_style = `
+  let link_button_style = `
   width: 30px;
   height: 30px;
   line-height: 20px;
   padding: 0;
   border: none;
-  background: rgb(255,27,0);
+  background: linear-gradient(0deg, rgba(255,27,0,1) 0%, rgba(251,75,2,1) 100%);
+  cursor: pointer;
+  border-radius: 5px;
+  margin-left: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+  let time_button_style = `
+  width: 30px;
+  height: 30px;
+  line-height: 20px;
+  padding: 0;
+  border: none;
   background: linear-gradient(0deg, rgba(255,27,0,1) 0%, rgba(251,75,2,1) 100%);
   cursor: pointer;
   border-radius: 5px;
@@ -124,105 +145,156 @@ let link_button_style = `
   align-items: center;
 `;
 
-// Create and append overlay element
-// const rr_logo = "./assets/redbarrewind.svg";
-const rr_logo = chrome.runtime.getURL("assets/rrewind.svg");
-const link_logo = chrome.runtime.getURL("assets/link.svg");
-const overlay = `
+  let link_button_style_dis = `
+  width: 30px;
+  height: 30px;
+  line-height: 20px;
+  padding: 0;
+  border: none;
+  background: linear-gradient(0deg, rgba(100,100,100,1) 0%, rgba(173, 173, 173,1) 100%);
+  cursor: pointer;
+  border-radius: 5px;
+  margin-left: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.25;
+`;
+
+  const rr_logo = chrome.runtime.getURL("assets/rrewind.svg");
+  const link_logo = chrome.runtime.getURL("assets/link.svg");
+  const time_logo = chrome.runtime.getURL("assets/time.svg");
+
+  const overlay = `
   <div id="rr_overlay" style="${overlay_style}">
     <img src="${rr_logo}" style="${image_style}"/>
     <form style="${form_style}" id="jumpForm">
-      <input style="${input_style}" autocomplete="off" type="text" id="timeInput" placeholder="00:00:00" />
+    <label for="timeInput"></label>
+      <input style="${input_style}" autocomplete="off" type="text" id="timeInput" class="timeInput" name="timeInput" placeholder="00:00:00" value=""/>
       <button style="${button_style}" type="submit" value="jump" name="action">Go</button>
-      <button style="${link_button_style}" type="submit" value="link" name="link"><img src="${link_logo}" style="${link_logo_style}"/></button>
+      <button style="${time_button_style}" type="submit" value="time" name="time"><img src="${time_logo}" style="${time_logo_style}"/></button>
+      <button style="${
+        domain.includes("youtube") ? link_button_style : "display: none;"
+      }" type="submit" value="link" name="link"><img src="${link_logo}" style="${link_logo_style}"/></button>
     </form>
   </div>`;
 
-const popupElement = document.createElement("div");
-popupElement.setAttribute("id", "popup_container")
-popupElement.setAttribute("class", "popup_container");
-popupElement.innerHTML = overlay;
-document.body.appendChild(popupElement);
+  const popupElement = document.createElement("div");
+  popupElement.id = "popup_container";
+  popupElement.className = "popup_container";
+  popupElement.innerHTML = overlay;
+  document.body.appendChild(popupElement);
 
-console.log("Plugged in.");
+  console.log("Plugged in.");
 
-const jumpForm = document.getElementById("jumpForm");
+  const jumpForm = document.getElementById("jumpForm");
 
-jumpForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  console.log("submitting jump");
-  manageTime(e);
-});
+  jumpForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("submitting jump");
+    manageTime(e);
+  });
 
-function manageTime(e) {
-  console.log(timeInput);
-  chrome.runtime.sendMessage(
-    { command: "checkActiveTab" },
-    function (response) {
-      if (response && response.isValid) {
-        const action = e.submitter.value;
-        console.log(action);
-        if (action === "jump") {
-          const timeInput = document
-            .getElementById("timeInput")
-            .value.split(":");
-          console.log(timeInput);
-          tl = timeInput.length;
+  function manageTime(e) {
+    // console.log(timeInput);
+    const action = e.submitter.value;
+    console.log(action);
+    if (action === "jump") {
+      const timeInput = document.getElementById("timeInput").value.split(":");
+      console.log(timeInput);
+      tl = timeInput.length;
 
-          let hoursInput, minutesInput, secondsInput, totalSeconds;
+      let hoursInput, minutesInput, secondsInput, totalSeconds;
 
-          if (tl === 3) {
-            hoursInput = parseInt(timeInput[0], 10) || 0;
-            minutesInput = parseInt(timeInput[1], 10) || 0;
-            secondsInput = parseInt(timeInput[2], 10) || 0;
-            totalSeconds = hoursInput * 3600 + minutesInput * 60 + secondsInput;
-          } else if (tl === 2) {
-            minutesInput = parseInt(timeInput[0], 10) || 0;
-            secondsInput = parseInt(timeInput[1], 10) || 0;
-            totalSeconds = minutesInput * 60 + secondsInput;
-          } else if (tl === 1) {
-            secondsInput = parseInt(timeInput[0], 10) || 0;
-            totalSeconds = secondsInput;
-          } else {
-            return;
-          }
+      if (tl === 3) {
+        hoursInput = parseInt(timeInput[0], 10) || 0;
+        minutesInput = parseInt(timeInput[1], 10) || 0;
+        secondsInput = parseInt(timeInput[2], 10) || 0;
+        totalSeconds = hoursInput * 3600 + minutesInput * 60 + secondsInput;
+      } else if (tl === 2) {
+        minutesInput = parseInt(timeInput[0], 10) || 0;
+        secondsInput = parseInt(timeInput[1], 10) || 0;
+        totalSeconds = minutesInput * 60 + secondsInput;
+      } else if (tl === 1) {
+        secondsInput = parseInt(timeInput[0], 10) || 0;
+        totalSeconds = secondsInput;
+      } else {
+        return;
+      }
 
-          // const hoursInput = parseInt(timeInput[2], 10) || 0;
-          // const minutesInput = parseInt(timeInput[1], 10) || 0;
-          // const secondsInput = parseInt(timeInput[0], 10) || 0;
-
-          // const totalSeconds = hoursInput * 3600 + minutesInput * 60 + secondsInput;
-          // console.log("hours: ", hoursInput);
-          // console.log("minutes: ", minutesInput);
-          // console.log("seconds: ", secondsInput);
-          // console.log("total: ", totalSeconds);
-
-          chrome.runtime.sendMessage({
+      chrome.runtime?.id
+        ? chrome.runtime.sendMessage({
             command: "jumpToTime",
             time: totalSeconds,
-          });
-          overlayVisibleBool = false;
-          toggleOverlay(overlayVisibleBool);
-        } else if (action === "link") {
-          const videoElement = document.querySelector("video");
-          let totalSeconds = videoElement.currentTime;
-          chrome.runtime.sendMessage({
-            command: "getLink",
-            time: totalSeconds,
-          });
-        }
+          })
+        : null;
+      overlayVisibleBool = false;
+      toggleOverlay(overlayVisibleBool);
+    } else if (action === "link") {
+      const videoElement = document.querySelector("video");
+      let totalSeconds = videoElement.currentTime;
+      if (domain.includes("youtube")) {
+        chrome.runtime?.id
+          ? chrome.runtime.sendMessage({
+              command: "getLink",
+              time: totalSeconds,
+            })
+          : null;
       }
+    } else if (action === "time") {
+      console.log("TIME STUFF!");
+      const videoElement = document.querySelector("video");
+      let currTime = videoElement.currentTime;
+
+      function secondsToTime(seconds) {
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const remainingSeconds = Math.floor(seconds % 60);
+
+        const formattedTime = `${padZero(hours)}:${padZero(minutes)}:${padZero(
+          remainingSeconds
+        )}`;
+        return formattedTime;
+      }
+
+      function padZero(num) {
+        return num < 10 ? `0${num}` : num;
+      }
+
+      // Example usage:
+      const formattedTime = secondsToTime(currTime);
+      console.log(formattedTime);
+
+      chrome.runtime?.id
+        ? chrome.runtime.sendMessage({
+            command: "copyTime",
+            time: formattedTime,
+          })
+        : null;
+    } else {
+      console.log("nothin'");
+      return;
     }
-  );
-}
-
-document.addEventListener("keydown", function (event) {
-  // Check if CTRL + ` is pressed
-  if (event.ctrlKey && event.key === "`") {
-    console.log("TOGGLE ACTIVATED!");
-    overlayVisibleBool = !overlayVisibleBool;
-    console.log("overlay: ", overlayVisibleBool);
-    toggleOverlay(overlayVisibleBool);
   }
-});
 
+  document.addEventListener("keydown", function (event) {
+    // Check if CTRL + ` is pressed
+    if (event.ctrlKey && event.altKey) {
+      document.getElementById("timeInput").value = "";
+      console.log("TOGGLE ACTIVATED!");
+      overlayVisibleBool = !overlayVisibleBool;
+      console.log("overlay: ", overlayVisibleBool);
+      toggleOverlay(overlayVisibleBool);
+    }
+  });
+
+  document.addEventListener(
+    "focusin",
+    function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      event.stopImmediatePropagation();
+    },
+    true
+  );
+})();
